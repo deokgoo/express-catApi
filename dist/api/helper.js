41c5c836d66be632b385602d1752294d41574dca
deokgoo <kkddgg1001@gmail.com>
2020-04-30 18:18:11 +0900

    remove heroku

--- dist/api/helper.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createQuery = function (queryObject) {
-    var query = [];
-    Object.entries(queryObject).forEach(function (x) {
-        query.push(x.join('='));
-    });
-    return query.join('&');
-};
